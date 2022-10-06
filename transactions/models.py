bf1a6de141c1ec69a62b1a76e68c50839a782202
Danilo <danvalerio89@gmail.com>
2022-10-06 11:39:21 -0300

    create: Model Store

--- transactions/models.py
+++ transactions/models.py
@@ -1,6 +1,5 @@
 import uuid
 
-from django.core.validators import MaxValueValidator
 from django.db import models
 
 
@@ -10,8 +9,4 @@ class Transaction(models.Model):
     type = models.CharField(max_length=1)
     date = models.DateField()
     value = models.FloatField()
-    cpf = models.CharField(max_length=11)
-    card = models.CharField(max_length=12)
     hour = models.TimeField()
-    store_owner = models.CharField(max_length=14)
-    store_name = models.CharField(max_length=19)
